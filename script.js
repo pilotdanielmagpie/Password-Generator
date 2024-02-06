89ac383f254a4891621e1ab8405bd4ffa81ac0e0
Daniel <danielm019695@gmail.com>
2024-02-06 14:47:43 -0800

    functions, id, and basic javascript variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
- 
+let inputEl = document.getElementById("input-el")
+let buttonEl = document.getElementById("generate-button")
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+function generate() {
+    let randomize = Math.floor(Math.random() * letters.length)
+    
+}
