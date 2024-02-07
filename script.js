ea40d4a7de607e81066c341a53027e08469412cb
Daniel <danielm019695@gmail.com>
2024-02-06 18:27:38 -0800

    figuring out the js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let inputEl = document.getElementById("input-el")
-let buttonEl = document.getElementById("generate-button")
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+let inputEl = document.getElementById("input-el");
+let buttonEl = document.getElementById("generate-button");
+let randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function generate() {
-    let randomize = Math.floor(Math.random() * letters.length)
-    
-}
+    for (let i = 0; i < 8; i++) {
+        let randomIndex = Math.floor(Math.random() * randomLetter.length);
+        return randomLetter
+        console.log(randomLetter[randomIndex])
+    }
+}     
+
+
+
